6ca2f5d7d42531fa6f8da7ba1673ff20205ddff5
gonglexin <gonglexin@gmail.com>
2013-03-28 09:13:57 +0800

    use form_for instead of form_tag for message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,6 +16,6 @@ class MessagesController < ApplicationController
 
   private
     def message_params
-      params.permit(:name, :contact, :body)
+      params.require(:message).permit(:name, :contact, :body)
     end
 end
